24f0d96620ec53d1f9443cf6469cf8db8d88515e
Peiyao Sun <sunpeiyao948@gmail.com>
2024-04-09 17:46:44 +0100

    update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,37 @@
-self.addEventListener("install", (event) => { 
-    console.log("Service worker installing..."); 
-    // Here, you'll later add code for actions like caching assets. 
-  });
+self.addEventListener("install", (event) => {
+  console.log("Service worker installing...");
+  // 在这里添加缓存资源的代码
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/src/components/FilterButton.jsx',
+        '/src/components/Form.jsx',
+        '/public/256x256.png',
+        '/public/duomi.png',
+        '/src/components/Todo.jsx',
+        '/src/App.css',
+        '/src/App.jsx',
+        '/src/db.jsx',
+        '/src/index.css',
+        '/src/main.jsx',
+        '/index.html',
+        '/vite.config.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+
+
+
+
+
