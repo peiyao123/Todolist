817f070e66a035a5e93da0b53e538dea6791367b
Peiyao Sun <sunpeiyao948@gmail.com>
2024-04-09 19:04:20 +0100

    update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,13 +21,13 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+ self.addEventListener('fetch', event => {
+   event.respondWith(
+    caches.match(event.request).then(response => {
+       return response || fetch(event.request);
+    })
+   );
+ });
 
 
 
